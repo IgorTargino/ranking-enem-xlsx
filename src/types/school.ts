9cb291e05e2ba522a57e47a01eb6e3087ea21623
igortargino <igortargino01@gmail.com>
2021-12-15 03:01:31 -0300

    chore(type): add school type

---
+++ src/types/school.ts
@@ -0,0 +1,24 @@
+type School = {
+  'Dependencia Administrativa': string;
+  LOCALIZACAO: string;
+  'Média (NOTA_REDACAO)': number;
+  // 'Média - ciências humanas': number;
+  // 'Média - linguagem': number;
+  // 'Média - matemática': number;
+  // 'Média Nota - ciências da natureza': number;
+  'Média da escola (provas objetivas)': number;
+  NO_ENTIDADE: string;
+  NO_MUNICIPIO_ESC: string;
+  // 'N_ALUNOS_EM - 3A': number;
+  // 'NÚMERO DE PARTICIPANTES NO ENEM': number;
+  PORTE_DA_ESCOLA: string;
+  // 'Posição (média Redação) Brasil': number;
+  // 'Posição (média Redação) UF': number;
+  'Posição (média provas objetivas) Brasil': number;
+  'Posição (média provas objetivas) UF': number;
+  SG_UF: string;
+  // SG_UF_ESC: string;
+  // 'TAXA DE PARTICIPAÇÃO': number;
+};
+
+export default School;
