4aa1094b7b7686052fb20715865b792dc14c5433
igortargino <igortargino01@gmail.com>
2021-12-15 03:04:30 -0300

    chore: lint fix

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,4 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 
 function AppRoutes() {
