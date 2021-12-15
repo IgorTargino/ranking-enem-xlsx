a1dfab052f56fb512a3f440c517ec2a16e1f3ee5
igortargino <igortargino01@gmail.com>
2021-12-15 03:07:19 -0300

    feat: add school context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import { SchoolContextProvider } from './context/SchoolContext';
 import AppRoutes from './routes';
 import GlobalStyles from './styles/global';
 
 function App() {
   return (
-    <>
+    <SchoolContextProvider>
       <GlobalStyles />
       <AppRoutes />
-    </>
+    </SchoolContextProvider>
   );
 }
 
