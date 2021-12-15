4650475a756b4c14563a75215b589f1f0d78df38
igortargino <igortargino01@gmail.com>
2021-12-15 19:03:11 -0300

    feat: add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import { ThemeProvider } from 'styled-components';
 import { SchoolContextProvider } from './context/SchoolContext';
 import AppRoutes from './routes';
 import GlobalStyles from './styles/global';
 
+import ligth from './styles/theme';
+
 function App() {
   return (
-    <SchoolContextProvider>
-      <GlobalStyles />
-      <AppRoutes />
-    </SchoolContextProvider>
+    <ThemeProvider theme={ligth}>
+      <SchoolContextProvider>
+        <GlobalStyles />
+        <AppRoutes />
+      </SchoolContextProvider>
+    </ThemeProvider>
   );
 }
 
