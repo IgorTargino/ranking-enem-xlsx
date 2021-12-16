1224e38062d22641e3fbf44d5070af4b5945a0e2
igortargino <igortargino01@gmail.com>
2021-12-16 05:00:10 -0300

    feat: add currentSchool, filteredSchools

--- src/context/SchoolContext.tsx
+++ src/context/SchoolContext.tsx
@@ -1,26 +1,72 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
+/* eslint-disable react/jsx-no-constructed-context-values */
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import School from '../types/school';
+import readExcel from '../utils/readExcel';
 
 interface SchoolContextProvideProps {
   children: ReactNode;
 }
 
 interface SchoolContextData {
-  allSchoolList: School[];
-  setAllSchoolList: (value: School[]) => void;
+  loading: boolean;
+  error: Error | undefined;
+  allSchools: School[] | undefined;
+  currentSchools: School[] | undefined;
+  filteredSchools: School[] | undefined;
+  getSchoolData: (value: File) => void;
+  setFilteredSchools: (value: School[] | undefined) => void;
+  setLoading: (value: boolean) => void;
 }
 
 const SchoolContext = createContext({} as SchoolContextData);
 
 const SchoolContextProvider = ({ children }: SchoolContextProvideProps) => {
-  const [allSchoolList, setAllSchoolList] = useState<School[]>([]);
+  const isFirstRender = useRef(true);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
+  const [allSchools, setAllSchools] = useState<School[]>();
+  const [currentSchools, setCurrentSchools] = useState<School[]>();
+  const [filteredSchools, setFilteredSchools] = useState<School[]>();
+
+  const getSchoolData = async (file: File) => {
+    try {
+      setLoading(true);
+      if (!file) throw new Error('Arquivo não encontrado!');
+      const data = await readExcel(file);
+      setAllSchools(data);
+    } catch (err: any) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (!isFirstRender.current) {
+      setCurrentSchools(allSchools?.slice(0, 5));
+    }
+
+    isFirstRender.current = false;
+  }, [allSchools]);
 
   return (
     <SchoolContext.Provider
-      // eslint-disable-next-line react/jsx-no-constructed-context-values
       value={{
-        allSchoolList,
-        setAllSchoolList,
+        loading,
+        error,
+        allSchools,
+        currentSchools,
+        filteredSchools,
+        getSchoolData,
+        setFilteredSchools,
+        setLoading,
       }}
     >
       {children}
